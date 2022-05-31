50996e4f95f2ef4d5d47fd4261e2ddaa1fee963b
David Stacey <daviddstacey1999@gmail.com>
2022-05-31 11:51:08 -0500

    ingress try 1 and 2

--- kubernetes-tf/2/ingress.tf
+++ kubernetes-tf/2/ingress.tf
@@ -1,4 +1,6 @@
-# resource "kubernetes_service" "kube-svc" {
+####### try number 1 error says
+####### Load Balancer is not ready yet
+# resource "kubernetes_service_v1" "ingress-svc" {
 #   metadata {
 #     name = "ingress-service"
 #   }
@@ -8,30 +10,111 @@
 #       target_port = 80
 #       protocol    = "TCP"
 #     }
-#     type = "NodePort"
+#     type = "LoadBalancer"
 #   }
 # }
 
-# resource "kubernetes_ingress" "ingress" {
+
+# resource "kubernetes_ingress_v1" "ingress" {
 #   wait_for_load_balancer = true
 #   metadata {
 #     name = "ingress"
-#     annotations = {
-#       "kubernetes.io/ingress.class" = "nginx"
-#     }
 #   }
 #   spec {
+#     ingress_class_name = "nginx"
 #     rule {
 #       http {
 #         path {
 #           path = "/*"
 #           backend {
-#             service_name = kubernetes_service.kube-svc.metadata.0.name
-#             service_port = 80
+#             service {
+#               #name = kubernetes_service.ingress-svc.metadata.0.name
+#               name = kubernetes_service_v1.ingress-svc.metadata.0.name
+#               port {
+#                 number = 80
+#               }
+#             }
 #           }
 #         }
 #       }
 #     }
 #   }
 # }
+
+###### this is try number 2 error 
+###### says Waiting for rollout to finish: 2 replicas wanted; 0 replicas Ready
+
+#-------------- CREATE DEPLOYMENT -----------------------------------------------------
+
+resource "kubernetes_deployment" "deploy" {
+  metadata {
+    name = "capstone"
+    labels = {
+      App = "capstone"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "capstone"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "capstone"
+        }
+      }
+      spec {
+        container {
+          image = "capstoneracr123:capstone-container"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+#-------------- CREATE LOAD BALANCER -----------------------------------------------------
+
+resource "kubernetes_service" "capstone-service" {
+  metadata {
+    name = "service"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.deploy.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+#-------------- OUTPUT PUBLIC IP -----------------------------------------------------
+
+output "lb_ip" {
+  value = kubernetes_service.capstone-service.status.0.load_balancer.0.ingress.0.ip
+}
+
 
